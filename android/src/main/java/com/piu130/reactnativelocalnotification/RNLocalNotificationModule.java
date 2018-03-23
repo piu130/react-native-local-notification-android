566964d188f792114046f04135d4b6129bbe3a5f
Ramon Boss <ramon.boss@outlook.com>
2018-03-23 10:57:41 +0100

    added import

--- android/src/main/java/com/piu130/reactnativelocalnotification/RNLocalNotificationModule.java
+++ android/src/main/java/com/piu130/reactnativelocalnotification/RNLocalNotificationModule.java
@@ -13,6 +13,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.NotificationManagerCompat;
 
 import java.util.Random;
 
