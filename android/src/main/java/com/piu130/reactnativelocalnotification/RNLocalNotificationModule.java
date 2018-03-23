f8283ef93dc2c97a7ec13ee418e8e92c9332b48b
Ramon Boss <ramon.boss@outlook.com>
2018-03-23 12:29:45 +0100

    fixed import

--- android/src/main/java/com/piu130/reactnativelocalnotification/RNLocalNotificationModule.java
+++ android/src/main/java/com/piu130/reactnativelocalnotification/RNLocalNotificationModule.java
@@ -8,16 +8,17 @@ import com.facebook.react.bridge.ReadableMap;
 
 import android.app.AlarmManager;
 import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.v4.app.NotificationManagerCompat;
 
 import java.util.Random;
 
 import static android.content.Context.ALARM_SERVICE;
+import static android.content.Context.NOTIFICATION_SERVICE;
 
 public class RNLocalNotificationModule extends ReactContextBaseJavaModule {
 
